c3e5da34b627f9a2048f38cc8bdf3438a05b2fe8
Christoph Mende <mende.christoph@gmail.com>
2012-07-05 19:32:16 +0200

    Add a distutils based build system

--- gtkvncviewer.py
+++ gtkvncviewer.py
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-version = "0.4"
+version = "0.5"
 
 import sys
 try:
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import glob
+from distutils.core import setup
+
+# TODO: locales
+setup(name='gtkvncviewer',
+        version='0.5',
+        description='Simple Gtk+ tool to connect to VNC server',
+        author='Clement Lorteau',
+        author_email='northern_lights@users.sourceforge.net',
+        url='http://cmende.github.com/gtkvncviewer',
+        scripts=['gtkvncviewer'],
+        license='GPL-2',
+        data_files=[('share/gtkvncviewer', ['gtkvncviewer.py']),
+            ('share/gtkvncviewer/data', glob.glob('data/*')),
+            ('share/applications', ['gtkvncviewer.desktop']),
+            ('share/man/man1', ['gtkvncviewer.1'])]
+        )
